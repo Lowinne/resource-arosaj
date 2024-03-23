90a5cec8e75cc4be4d67029a8889ba24487dcc71
BXSoft <jeremiahlowinne@gmail.com>
2024-03-23 13:04:18 +0100

    First commit

---
+++ src/main/java/com/epsi/arosaj/ArosajApplication.java
@@ -0,0 +1,13 @@
+package com.epsi.arosaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArosajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArosajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epsi/arosaj/ArosajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsi.arosaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArosajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
