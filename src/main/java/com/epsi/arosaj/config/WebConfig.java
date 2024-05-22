a5f34fbe6e4a8f3cb325e5aa414311ac684d28f3
Lux VEGBA <71674056+iimAtomic@users.noreply.github.com>
2024-05-22 18:41:24 +0200

    Update WebConfig.java

--- src/main/java/com/epsi/arosaj/config/WebConfig.java
+++ src/main/java/com/epsi/arosaj/config/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//Lulu: web config pour coRs :)
+//Luluu: web config pour coRs :)
 @Configuration
 public class WebConfig {
 
